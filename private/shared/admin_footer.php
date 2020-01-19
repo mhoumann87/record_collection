e53666c1723d3846c99332dfffbbb99214dee399
Michael Houmann <mhoumann87@gmail.com>
2020-01-19 19:45:24 +0100

    Add admin-footer and styles, change color scheme

--- private/shared/admin_footer.php
+++ private/shared/admin_footer.php
@@ -1,5 +1,8 @@
-<footer>
-  <h3>This is the Admin Footer</h3>
+<footer class="admin-footer">
+  <div class="first">
+    <h4>My Record Collection</h4>
+    <small>&copy;<?php echo (2020 == date("Y")) ? '2020' : "2020 - " . date("Y"); ?> <a href="https://michael-h.dk" target="_blank">Michael Houmann</a></small>
+  </div>
 </footer>
 
 <script src="<?php echo url_for('/assets/js/main.js'); ?>"></script>
