32bae3b55ab01aa87f016cfbd7f7a89feafd75e4
Michael Houmann <mhoumann87@gmail.com>
2020-01-24 19:47:41 +0100

    Add form-fields.php for grades

---
+++ public/admin/grades/form_fields.php
@@ -0,0 +1,39 @@
+<?php
+
+// Prevents this code from being loaded in the browser
+// withput first setting the nessary object
+
+if (!isset($grade)) {
+  redirect_to(url_for('/admin/grades/index.php'));
+}
+?>
+
+<div class="input-box">
+
+  <label for="value">Value of the grading: </label>
+  <input type="text" name="grade[value]" value="<?php echo h($grade->value); ?>" autofocus>
+
+</div>
+
+<div class="input-box">
+
+  <label for="short">Short name: </label>
+  <input type="text" name="grade[short]" value="<?php echo h($grade->value); ?>">
+
+</div>
+
+<div class="input-box">
+
+  <label for="description">Definition: </label>
+  <textarea name="grade[definition]">
+    <?php echo h($grade->definition); ?>
+  </textarea>
+
+</div>
+
+<div class="input-box">
+
+  <label for="image">Link to image:</label>
+  <input type="text" name="grade[image]" value="<?php echo h($grade->image); ?>">
+
+</div>
--- public/admin/grades/new.php
+++ public/admin/grades/new.php
@@ -4,16 +4,33 @@
 
 <?php
 
-// If it is a get request, just show the form with an empty post
+if (is_post_request()) {
+  // If it is a post request instianziate a new grade
+} else {
 
-$grade = new Grade;
+  // If it is a get request, just show the form with an empty post
+
+  $grade = new Grade;
+}
 ?>
 
 <?php include SHARED_PATH . '/admin_header.php'; ?>
 
+<a href="<?php echo url_for('/admin/grades/index.php'); ?>">
+  <button class="btn-link">&larr;Back To List</button>
+</a>
 <section class="input-page">
 
   <?php echo display_errors($grade->errors); ?>
+
+  <form action="<?php echo url_for('/admin/grades/new.php'); ?>" method="post">
+
+    <?php include 'form_fields.php' ?>
+
+    <div class="input-box">
+      <input type="submit" class="button btn-success" value="Create Grade">
+    </div>
+  </form>
 
 </section>
 
