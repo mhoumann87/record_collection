3d8bcf2d0fac1b0fde0b336d46fd04761e7a5c8d
Michael Houmann <mhoumann87@gmail.com>
2020-02-13 13:43:38 +0100

    Add start of artist class, and index.php in artists

---
+++ private/classes/Artist.class.php
@@ -0,0 +1,50 @@
+<?php
+
+class Artist extends DatabaseObject
+{
+
+  static protected $table_name = 'artists';
+  static protected $db_columns = [
+    'id',
+    'bandname',
+    'the_first',
+    'firstname',
+    'lastname',
+    'image',
+    'profile',
+    'website',
+    'amazon_link'
+  ];
+  public $allowed_tags = [
+    '<br>',
+    '<p>',
+    '<h1>',
+    '<h2>',
+    '<h3>',
+    '<h4>',
+    '<h5>',
+    '<h6>',
+  ];
+
+  public $id;
+  public $brandname;
+  public $the_first;
+  public $firstname;
+  public $lastname;
+  public $image;
+  public $profile;
+  public $website;
+  public $amazon_link;
+
+  public function __construct($args = [])
+  {
+    $this->brandname = $args['bandname'] ?? '';
+    $this->the_first = $args['the_first'] ?? 0;
+    $this->firstname = $args['firstname'] ?? '';
+    $this->lastname = $args['lastname'] ?? '';
+    $this->image = $args['image'] ?? '';
+    $this->profile = $args['profile'] ?? '';
+    $this->website = $args['website'] ?? '';
+    $this->amazon_link = $args['amazon_link'] ?? '';
+  }
+} // class
