0767b7407e91319b90203b78ced0b4ed0fe57e15
Michael Houmann <mhoumann87@gmail.com>
2020-03-19 20:01:16 +0100

    Add form_fields.php ot wishlist and add variables to class

--- private/classes/Wishlist.class.php
+++ private/classes/Wishlist.class.php
@@ -10,4 +10,16 @@ class Wishlist extends DatabaseObject
     'record_id',
     'format_id'
   ];
-}
+
+  public $id;
+  public $user_id;
+  public $record_id;
+  public $format_id;
+
+  public function __construct($args = [])
+  {
+    $this->user_id = $args['user_id'] ?? '';
+    $this->record_id = $args['record_id'] ?? '';
+    $this->format_id = $args['format_id'] ?? '';
+  }
+} // class
